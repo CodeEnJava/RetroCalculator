a7daee0e709a527e9f9c56890d6f2e8414627fbe
stebar <stebar@CRP-22K-AF.ladapt.local>
2025-08-19 13:41:33 +0200

    ajouter classe pour tests MyJButton et Display

---
+++ src/codeEnJava/test_1/KeyExample.java
@@ -0,0 +1,81 @@
+package codeEnJava.test_1;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.math.BigDecimal;
+
+import codeEnJava.displays.Display;
+import codeEnJava.keyboard.MyJButton;
+
+public class KeyExample {
+	private MyJButton key = null;
+	private Display display = null;
+	private String val = null;
+	
+	public KeyExample(int width, int height, String val, Display display) {
+		super();
+		this.display = display;
+		this.val = val;
+		
+		key = new MyJButton(""+val.charAt(0), width,height);
+		
+		key.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int int_count = 0;
+				float float_count = 0.0f;
+				if(key.isPressed()) {
+					
+					switch(key.getValue()) {
+					
+					case "+":	
+							if( display.getStr_val().contains(".")) {
+								BigDecimal a = new BigDecimal(display.getStr_val());
+								BigDecimal b = new BigDecimal("0.5");
+								BigDecimal result = a.add(b);
+								float_count = result.floatValue();
+								display.clear();
+								display.setValue(String.valueOf(float_count));
+							}else {
+								int_count = Integer.parseInt(display.getStr_val().trim())+1;
+								display.setValue(""+int_count);
+							}
+					
+							break;
+							
+					case "-":	
+							if( display.getStr_val().contains(".")) {
+								BigDecimal a = new BigDecimal(display.getStr_val());
+								BigDecimal b = new BigDecimal("0.5");
+								BigDecimal result = a.subtract(b);
+								float_count = result.floatValue();
+								display.clear();
+								display.setValue(String.valueOf(float_count));
+							}else {
+								int_count = Integer.parseInt(display.getStr_val().trim())-1;
+								display.setValue(""+int_count);
+							}
+					
+							break;
+					
+					case "C":
+							display.clear();
+							display.setValue("0.0");
+							break;
+					default:
+						
+					}
+					
+				}
+			}
+		});
+	}
+
+	public MyJButton getKey() {
+		return key;
+	}
+	
+	
+	
+
+}
